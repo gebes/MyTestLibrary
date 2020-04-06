357cc3110f7b90f8cce3b838506d39f71f248d83
Christoph Krassnigg <christoph_krassnigg@gmx.net>
2020-04-06 14:32:20 +0200

    Smoother Scrolling for the Header

--- sticky_header.js
+++ sticky_header.js
@@ -13,6 +13,7 @@ window.onload = function (){
     sticky = sticky_header.offsetTop;
 };
 
+
 // Add the sticky class to the sticky_header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset > sticky) {
